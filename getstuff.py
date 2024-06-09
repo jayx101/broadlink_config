b2cd23d6d2fe01da95b257602fff66be3e1cf460
jayx101 <oelofsej@gmail.com>
2024-06-09 13:34:11 +0800

    Converted to Python3

--- getstuff.py
+++ getstuff.py
@@ -1,68 +1,55 @@
-
-import simplejson as json
-
-
-buttonIDS = []
-buttonNames = []
-
-
-jsonSubIr = open("jsonSubIr").read()
-jsonSubIrData = json.loads(jsonSubIr)
-
-
-for i in range(0, len(jsonSubIrData)):
-    print "ID:", jsonSubIrData[i]['id'], "| Name:", jsonSubIrData[i]['name']
-
-
-choice = input("Select accessory ID: ")
-
-for i in range(0, len(jsonSubIrData)):
-    if jsonSubIrData[i]['id'] == choice:
-        accessory_name = jsonSubIrData[i]['name']
-        print "[+] You selected: ", accessory_name
-
-
-jsonButton = open("jsonButton").read()
-jsonButtonData = json.loads(jsonButton)
-
-
-for i in range(0, len(jsonButtonData)):
-    if jsonButtonData[i]['subIRId'] == choice:
-        buttonIDS.append(jsonButtonData[i]['id'])
-        buttonNames.append(jsonButtonData[i]['name'])
-
-
-jsonIrCode = open("jsonIrCode").read()
-jsonIrCodeData = json.loads(jsonIrCode)
-
-
-print "[+] Dumping codes to " + accessory_name + ".txt"
-print "[+] Dumping yaml to " + accessory_name + ".yaml"
-
-
-codesFile = open(accessory_name + '.txt', 'w')
-yamlFile = open(accessory_name + '.yaml', 'w')
-
-groupstext = "\n\n\ngroup:\n"
-groupstext = groupstext + "  " + accessory_name + ":\n"
-groupstext = groupstext + "    name: " + accessory_name + "\n"
-groupstext = groupstext + "    view: yes\n"
-groupstext = groupstext + "    entities:\n"
-
-yamltext = "    switches:\n"
-
-for i in range(0, len(jsonIrCodeData)):
-    for j in range(0, len(buttonIDS)):
-        if jsonIrCodeData[i]['buttonId'] == buttonIDS[j]:
-            code = ''.join('%02x' % (i & 0xff) for i in jsonIrCodeData[i]['code'])
-            code_b64 = code.decode("hex").encode("base64")
-            result = "Button Name: " + buttonNames[j] + " | Button ID: " + str(jsonIrCodeData[i]['buttonId']) + " | Code: " + code
-            namepart = buttonNames[j].replace(" ","_")
-            yamltext = yamltext + "      " + accessory_name + "_" + namepart + ":\n"
-            yamltext = yamltext + "        friendly_name: " + accessory_name + " " + buttonNames[j] + "\n"
-            yamltext = yamltext + "        command_on: '" + code_b64[:-1] + "'" + "\n"
-            groupstext = groupstext + "      - switch." + accessory_name + "_" + namepart + "\n"
-            codesFile.writelines(result.encode('utf-8'))
-yamlFile.writelines(yamltext.encode('utf-8'))
-yamlFile.writelines(groupstext.encode('utf-8'))
-
+import simplejson as json  
+import base64  
+  
+buttonIDS = []  
+buttonNames = []  
+  
+# Reading JSON data  
+with open("jsonSubIr", "r") as file:  
+    jsonSubIrData = json.load(file)  
+  
+# Printing ID and Name  
+for item in jsonSubIrData:  
+    print("ID:", item['id'], "| Name:", item['name'])  
+  
+choice = input("Select accessory ID: ")  
+  
+accessory_name = ""  
+for item in jsonSubIrData:  
+    if str(item['id']) == choice:  
+        accessory_name = item['name']  
+        print("[+] You selected: ", accessory_name)  
+  
+with open("jsonButton", "r") as file:  
+    jsonButtonData = json.load(file)  
+  
+for item in jsonButtonData:  
+    if str(item['subIRId']) == choice:  
+        buttonIDS.append(item['id'])  
+        buttonNames.append(item['name'])  
+  
+with open("jsonIrCode", "r") as file:  
+    jsonIrCodeData = json.load(file)  
+  
+print("[+] Dumping codes to " + accessory_name + ".txt")  
+print("[+] Dumping yaml to " + accessory_name + ".yaml")  
+  
+# Writing to files  
+with open(accessory_name + '.txt', 'w') as codesFile, open(accessory_name + '.yaml', 'w') as yamlFile:  
+    groupstext = "\n\n\ngroup:\n  " + accessory_name + ":\n    name: " + accessory_name + "\n    view: yes\n    entities:\n"  
+    yamltext = "    switches:\n"  
+  
+    for i in range(len(jsonIrCodeData)):  
+        for j in range(len(buttonIDS)):  
+            if jsonIrCodeData[i]['buttonId'] == buttonIDS[j]:  
+                code_hex = ''.join('%02x' % (byte & 0xff) for byte in jsonIrCodeData[i]['code'])  
+                code_b64 = base64.b64encode(bytes.fromhex(code_hex)).decode('utf-8')  
+                result = "Button Name: " + buttonNames[j] + " | Button ID: " + str(jsonIrCodeData[i]['buttonId']) + " | Code: " + code_hex  
+                namepart = buttonNames[j].replace(" ", "_")  
+                yamltext += "      " + accessory_name + "_" + namepart + ":\n"  
+                yamltext += "        friendly_name: " + accessory_name + " " + buttonNames[j] + "\n"  
+                yamltext += "        command_on: '" + code_b64 + "'\n"  
+                groupstext += "      - switch." + accessory_name + "_" + namepart + "\n"  
+                codesFile.write(result + '\n')  
+    yamlFile.write(yamltext)  
+    yamlFile.write(groupstext)  
